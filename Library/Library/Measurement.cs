0d3ada4159c557bdfc964f81cbd0a50675ce153e
Kai Bergmann <90706342+KaiBerg@users.noreply.github.com>
2022-10-24 13:42:29 +0200

    added class library

---
+++ Library/Library/Measurement.cs
@@ -0,0 +1,11 @@
+﻿namespace Library
+{
+    public class Measurement
+    {
+        public int MeterId { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public float Value { get; set; }
+
+    }
+}
